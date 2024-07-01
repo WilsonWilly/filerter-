6ff7ca0d21cc33bde44d4c1e24ac22a43c027387
jdr-dev-dreadcast <jdr.dev.dreadcast@gmail.com>
2024-07-01 16:24:06 +0200

    ajust css Layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,6 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-
     <html lang="fr">
       <body className={adventureFont.className}>
         <div className="md:container md:mx-auto md:w-[85%] containerMain">
