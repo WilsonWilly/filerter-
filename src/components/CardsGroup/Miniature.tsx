7b24f8518161c4e97428665038b9855a1e5a6845
Yoann-Guion <yoo.guion@gmail.com>
2024-07-01 16:02:25 +0200

    add responsiveness for homePage : sm md and lg

--- src/components/CardsGroup/Miniature.tsx
+++ src/components/CardsGroup/Miniature.tsx
@@ -1,29 +1,34 @@
-import { Card, Image, CardFooter } from "@nextui-org/react";
+import { Card, Image, CardFooter } from '@nextui-org/react';
 
 interface MiniatureProps {
-    name: string,
-    description: string,
-    picture: string,
-
+  name: string;
+  description: string;
+  picture: string;
 }
 
-function Miniature({name, description, picture}: MiniatureProps) {
-    return (
-        <Card isFooterBlurred fullWidth radius="lg"
-        className="m-1 h-32 md:w-60 md:h-60">
-            <Image isZoomed
-                removeWrapper
-                alt="Boardgame cover"
-                className="z-0 w-full h-full object-cover"
-                src={picture}
-            />
-            <CardFooter className="absolute z-10 bottom-0 flex-col !items-start bg-white/35 rounded-lg">
-                <p></p>
-                <h3 className="text-medium text-black uppercase font-bold">{name}</h3>
-                <h4 className="text-grey font-light text-xs">{description}</h4>
-            </CardFooter>
-        </Card>
-    )
+function Miniature({ name, description, picture }: MiniatureProps) {
+  return (
+    <Card
+      isFooterBlurred
+      fullWidth
+      radius="lg"
+      className="m-1 h-32 sm:w-40 sm:h-40 md:w-50 md:h-50 lg:w-60 lg:h-60"
+    >
+      <Image
+        isZoomed
+        removeWrapper
+        alt="Boardgame cover"
+        className="z-0 w-full h-full object-cover md:object-top"
+        src={picture}
+      />
+      <CardFooter className="absolute z-10 bottom-0 flex-col !items-start bg-white/35 rounded-lg">
+        <h3 className="text-black text-medium uppercase font-bold">{name}</h3>
+        <h4 className="text-blueNight font-extralight text-xs truncate overflow-hidden ... h-1/5 w-full">
+          {description}
+        </h4>
+      </CardFooter>
+    </Card>
+  );
 }
 
 export default Miniature;
