159e6bd0ac9fadb1cbe0e859a9fd37f082e2170e
jdr-dev-dreadcast <jdr.dev.dreadcast@gmail.com>
2024-07-02 16:51:08 +0200

    amelioration detailGame again

--- src/app/[nomJeuxDeSociete]/page.tsx
+++ src/app/[nomJeuxDeSociete]/page.tsx
@@ -29,42 +29,46 @@ export default function BoardGame() {
       <section className="w-full py-8 text-center mb-4 border border-black rounded-md p-4">
         <p>{detailsGame.game.description} </p>
         <p>Difficulté : {detailsGame.game.difficulty}</p>
+        <p>Age minimum : {detailsGame.game.minAge} ans</p>
+        <p>Temps d'une partie : {detailsGame.game.time}</p>
+        <p>
+          Nombre de joeur : de {detailsGame.game.minPlayers} à
+          {detailsGame.game.maxPlayers} Joueurs
+        </p>
       </section>
       <section className="w-full flex flex-col items-center space-y-4 ">
         <div className="w-full flex justify-around space-x-4">
           <Button
             // onPress={actionAjoutCree}
-            className=" flex items-center border border-black  active:black"
-            radius="sm"
-            startContent={<PlusSquare className="w-20 h-20" />}
+            className=" border border-black  active:black"
+            startContent={<PlusSquare />}
           >
             Ajouter à ma bibliothéque
           </Button>
 
           <Button
             // onPress={actionAjoutCree}
-            className=" flex items-center border border-black  active:black"
-            radius="sm"
-            startContent={<PlusSquare className="w-20 h-20" />}
+            className="  border border-black  active:black"
+            startContent={<PlusSquare />}
           >
             Ajouter à ma liste de souhait
           </Button>
         </div>
         <div className="w-full flex items-center justify-around space-x-4">
           <ModalOpinion />
-          <div className="w-3/4  p-1 overflow-y-auto h-40 border border-black rounded-md">
+          <div className="w-3/4 bg-blueNight p-1 overflow-y-auto h-40 border border-black rounded-md">
             {detailsGame.opinions.map((opinion) => (
               <div
-                className="p-2  mb-1 last:mb-0 border border-black rounded-md p-1"
+                className=" bg-orangePastel mb-1 last:mb-0 border border-black rounded-md p-1"
                 key={opinion.id}
               >
                 <div className="flex justify-between mb-2 ">
-                  <div>{opinion.pseudo}</div>
-                  <div>
-                    <Rating goldRating={opinion.rating} />
+                  <div className=" flex items-center text-lg">
+                    {opinion.pseudo}
                   </div>
+                  <Rating goldRating={opinion.rating} />
                 </div>
-                <div>{opinion.comment}</div>
+                <div className="text-sm">{opinion.comment}</div>
               </div>
             ))}
           </div>
