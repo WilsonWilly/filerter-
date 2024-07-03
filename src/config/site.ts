7596a4627b1f38d7b88622fc08676d00d7788b6a
WIlsonWilly <wilsonrabbitreseaux@gmail.com>
2024-07-03 15:55:38 +0200

    links edited

--- src/config/site.ts
+++ src/config/site.ts
@@ -56,9 +56,8 @@ export const siteConfig = {
     },
   ],
   links: {
-    twitter: 'https://twitter.com/getnextui',
-    docs: 'https://nextui.org',
-    discord: 'https://discord.gg/9b6yyZKmH4',
-    sponsor: 'https://patreon.com/jrgarciadev',
+    twitter: 'https://x.com/',
+    discord: 'https://discord.com/',
+    sponsor: '',
   },
 };
