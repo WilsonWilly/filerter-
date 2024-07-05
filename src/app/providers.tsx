ca18371384427dd55bcb47722edf29e1e8827de1
jdr-dev-dreadcast <jdr.dev.dreadcast@gmail.com>
2024-07-05 10:09:24 +0200

    uptade correction for deploy

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -3,16 +3,14 @@
 import * as React from "react";
 import { NextUIProvider } from "@nextui-org/system";
 import { useRouter } from "next/navigation";
-import { ThemeProviderProps } from "next-themes/dist/types";
 import { Provider as ReduxProvider } from "react-redux";
 import store from "./lib/store";
 
 export interface ProvidersProps {
   children: React.ReactNode;
-  themeProps?: ThemeProviderProps;
 }
 
-export function Providers({ children, themeProps }: ProvidersProps) {
+export function Providers({ children }: ProvidersProps) {
   const router = useRouter();
 
   return (
